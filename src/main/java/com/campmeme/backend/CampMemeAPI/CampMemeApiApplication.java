c8daf4e5b75aabc9b2dbf7c4ec52a22d87cc44e3
Steven HENRY <stevenhenry83270@gmail.com>
2023-12-11 16:07:53 +0100

    First commit

---
+++ src/main/java/com/campmeme/backend/CampMemeAPI/CampMemeApiApplication.java
@@ -0,0 +1,13 @@
+package com.campmeme.backend.CampMemeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampMemeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampMemeApiApplication.class, args);
+	}
+
+}
